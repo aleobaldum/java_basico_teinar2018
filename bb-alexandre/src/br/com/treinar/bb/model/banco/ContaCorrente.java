5d3e26c2a94e298ec916d6822b4ad66d090c1f0f
aleobaldum <40009913+aleobaldum@users.noreply.github.com>
2018-06-26 13:48:33 -0300

    enun

--- bb-alexandre/src/br/com/treinar/bb/model/banco/ContaCorrente.java
+++ bb-alexandre/src/br/com/treinar/bb/model/banco/ContaCorrente.java
@@ -1,5 +1,7 @@
 package br.com.treinar.bb.model.banco;
 
+import br.com.treinar.bb.model.tipodado.TipoCliente;
+
 public class ContaCorrente extends Conta implements IProdutoPagavel {
 
 	private double va1orPagar;
